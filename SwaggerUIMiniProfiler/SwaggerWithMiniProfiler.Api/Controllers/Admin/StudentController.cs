eeba3102db2846b9526c893501b47814f978db1c
ChuanmingXie <ChuanmingXie@outlook.com>
2022-07-01 08:20:18 +0800

    BlogCoreV2-创建WebAPI项目,为前后端分离提供接口支持

---
+++ SwaggerUIMiniProfiler/SwaggerWithMiniProfiler.Api/Controllers/Admin/StudentController.cs
@@ -0,0 +1,90 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using SwaggerWithMiniProfiler.BLL.Admin;
+using SwaggerWithMiniProfiler.Model.Entities;
+
+namespace SwaggerWithMiniProfiler.Api.Controllers.Admin
+{
+    /// <summary>
+    /// 学生模块
+    /// </summary>
+    [Produces("application/json")]
+    [Route("api/admin/[controller]")]
+    public class StudentController : Controller
+    {
+        private StudentBLL studentBLL = new StudentBLL();
+
+        /// <summary>
+        /// 获取学生分页列表
+        /// </summary>
+        /// <param name="pageIndex"></param>
+        /// <param name="pageSize"></param>
+        /// <returns></returns>
+        [HttpGet]
+        [Route("GetAllStudent")]
+        [Authorize(Roles = "Admin")]
+        [Authorize(Roles = "System")]
+        public JsonResult GetStudentPageList(int pageIndex = 1, int pageSize = 10)
+        {
+            return Json(studentBLL.GetPageList(pageIndex, pageSize));
+        }
+
+        /// <summary>
+        /// 获取单个学生
+        /// </summary>
+        /// <param name="id"></param>
+        /// <returns></returns>
+        [HttpGet, Route("GetStudentByID/{id}")]
+        [Authorize]
+        public JsonResult GetStudentById(long id)
+        {
+            return Json(studentBLL.GetById(id));
+        }
+
+        /// <summary>
+        /// 添加学生
+        /// </summary>
+        /// <param name="entity"></param>
+        /// <returns></returns>
+        [HttpPost]
+        [Route("AddStudent")]
+        [Authorize(Policy = "SystemOrAdmin")]
+        public JsonResult Add(Student entity = null)
+        {
+            if (entity == null)
+                return Json("参数为空");
+
+            return Json(studentBLL.Add(entity));
+        }
+
+        /// <summary>
+        /// 编辑学生
+        /// </summary>
+        /// <param name="entity"></param>
+        /// <returns></returns>
+        [HttpPut]
+        [Route("UpdateStudent")]
+        [Authorize(Roles = "Admin")]
+        public JsonResult Update(Student entity = null)
+        {
+            if (entity == null)
+                return Json("参数为空");
+
+            return Json(studentBLL.Update(entity));
+        }
+
+        /// <summary>
+        /// 删除学生
+        /// </summary>
+        /// <param name="ids"></param>
+        /// <returns></returns>
+        [HttpDelete]
+        [Route("DeleteStudent")]
+        public JsonResult Delete(dynamic[] ids = null)
+        {
+            if (ids.Length == 0)
+                return Json("参数为空");
+            return Json(studentBLL.Dels(ids));
+        }
+    }
+}
---
+++ SwaggerUIMiniProfiler/SwaggerWithMiniProfiler.Api/Controllers/Admin/WeatherController.cs
@@ -0,0 +1,53 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using StackExchange.Profiling;
+using SwaggerWithMiniProfiler.BLL.Admin;
+using SwaggerWithMiniProfiler.Model.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace SwaggerWithMiniProfiler.Api.Admin.Controllers
+{
+    /// <summary>
+    /// 天气模块
+    /// </summary>
+    [Produces("application/json")]
+    [Route("api/Admin/[controller]")]
+    public class WeatherController : ControllerBase
+    {
+        private readonly IHttpContextAccessor _accessor;
+        private readonly ILogger<WeatherController> _logger;
+        private readonly WeatherBLL weatherBLL = new WeatherBLL();
+
+        public WeatherController(ILogger<WeatherController> logger, IHttpContextAccessor accessor)
+        {
+            _logger = logger;
+            _accessor = accessor;
+        }
+
+        /// <summary>
+        /// 获取天气
+        /// </summary>
+        /// <returns></returns>
+        [HttpGet]
+        [Route("GetWeatherInfo")]
+        public IEnumerable<Weather> Get()
+        {
+            return weatherBLL.GetWeathers();
+        }
+
+        /// <summary>
+        /// 获取MiniProfiler头文件
+        /// </summary>
+        [HttpGet]
+        [Produces("text/plain")]
+        [Route("GetHeaderSript")]
+        public IActionResult SwaggerUIHeader()
+        {
+            var result = MiniProfiler.Current.RenderIncludes(_accessor.HttpContext);
+            return Ok(result.Value);
+        }
+    }
+}
